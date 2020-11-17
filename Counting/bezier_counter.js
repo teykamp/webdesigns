71947daa80e0ef118e65cfbf5588a6e92a511d2b
eykamt <embergameslp@gmail.com>
2020-11-17 11:06:10 -0500

    Updated Comments
    Fixed organization too

--- Counting/bezier_counter.js
+++ Counting/bezier_counter.js
@@ -1,4 +1,4 @@
-//cvega15 @ github
+// major help from cvega15 @ github
 
 class cubic_bezier {
     constructor(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y) {
@@ -27,40 +27,41 @@ class cubic_bezier {
 };
 
 // bezier_counter - special increment function that outputs a linear interpolation between two numbers over time.
-// element -
-// timestamp -
-// duration -
+// start_time - timestamp of initial object creation (see bottom: start animation)
+// element - element you want to change
+// timestamp - timestamp of object creation (does not reset until page reload!)
+// duration - time of animation
 // start_number - the initial value to count from
 // end_number - the final value to reach within the time constraint
-// animation_bezier -
+// animation_bezier - animation
 function bezier_counter(timestamp, start_time, element, duration, start_number, end_number, animation_bezier) {
     var progress = timestamp - start_time;
 
-    //var bezier_y = animation_bezier.get_y(timestamp/duration) * end_number;
+    // var bezier_y = animation_bezier.get_y(timestamp/duration) * end_number;
     var bezier_y = animation_bezier.get_y(progress / duration) * (end_number - start_number) + start_number;
     element.innerHTML = Math.floor(bezier_y);
 
-    //check to see if function finished execution
-    if(progress < duration) {
+    // check to see if function finished execution
+    if (progress < duration) {
         requestAnimationFrame(function(timestamp) {
             bezier_counter(timestamp, start_time, element, duration, start_number, end_number, animation_bezier);
         });
     } else {
-        //change to the users end_number at the end in case something went wrong
+        // change to the users end_number at the end in case something went wrong
         element.innerHTML = end_number;
     };
 };
 
 function bezier_animation(element_id, start_number=0, end_number, animation_duration, p1_x=null, p1_y=null, p2_x=null, p2_y=null) {
 
-    //set default coordinates
+    // set default coordinates
     var p1 = {p1_x: .01, p1_y: 1};
     var p2 = {p2_x: .01, p2_y: 1};
 
-    //if the user inputted bezier coordinates then check to make sure they are in range, if so then change the default coordinates to the user coordinates
-    if(p1_x && p1_y && p2_x && p2_y) {
-        if(p1_x > 1 || p1_x < 0 || p2_x > 1 || p2_x < 0) {
-            throw "p1_x and p2_x have to be between 1 and 0, please check you got this right";
+    // if the user inputted bezier coordinates then check to make sure they are in range, if so then change the default coordinates to the user coordinates
+    if (p1_x && p1_y && p2_x && p2_y) {
+        if (p1_x > 1 || p1_x < 0 || p2_x > 1 || p2_x < 0) {
+            throw "p1_x and p2_x have to be between 1 and 0";
         } else {
             console.log('using customs')
             p1 = {p1_x: p1_x, p1_y: p1_y};
@@ -68,17 +69,17 @@ function bezier_animation(element_id, start_number=0, end_number, animation_dura
         };
     };
 
-    //get the element to change by id
+    // get the element to change by id
     var element = document.getElementById(element_id);
 
-    //create a new bezier object
+    // create a new bezier object
     const animation_bezier = new cubic_bezier(0, 0, p1.p1_x, p1.p1_y, p2.p2_x, p2.p2_y, 1, 1);
 
-    //start animation
+    // start animation
     requestAnimationFrame(function(timestamp) {
         var start_time = timestamp;
         bezier_counter(timestamp, start_time, element_id, animation_duration, start_number, end_number, animation_bezier)
     });
 };
 
-//export {bezier_animation};
+// export {bezier_animation};
