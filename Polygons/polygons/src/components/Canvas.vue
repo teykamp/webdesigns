a818a1e8ea1317b8ab2e36ba5a17d0a47652db45
Thomas Eykamp <embergameslp@gmail.com>
2021-03-15 01:11:08 -0400

    Moving triangle

--- Polygons/polygons/src/components/Canvas.vue
+++ Polygons/polygons/src/components/Canvas.vue
@@ -16,38 +16,60 @@ export default {
         return Math.random() * (max - min) + min; 
     }
 
+    // admin
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     canvas.height = window.innerHeight-20;
     canvas.width = window.innerWidth-20;
-    var ballRadius = 5;
-    var x = randomNumber(0,canvas.width);
-    var y = randomNumber(0,canvas.height);
-    var dx = randomNumber(-.5, .5)
-    var dy = randomNumber(-.5, .5)
+    const ballRadius = 5;
 
-    function drawBall() {
+    class Ball {
+      constructor(x, y, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+      }
+
+      drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-        ctx.fillStyle = "#000000";
+        ctx.arc(this.x, this.y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#000";
         ctx.fill();
         ctx.closePath();
+        if(this.x + this.dx > canvas.width-ballRadius || this.x + this.dx < ballRadius) {
+            this.dx = -this.dx;
+        }
+        if(this.y + this.dy > canvas.height-ballRadius || this.y + this.dy < ballRadius) {
+            this.dy = -this.dy;
+        }
+        
+        this.x += this.dx;
+        this.y += this.dy;
+      }
     }
+    const b1 = new Ball(randomNumber(0,canvas.width), randomNumber(0,canvas.height), randomNumber(-.5, .5), randomNumber(-.5, .5));
+    const b2 = new Ball(randomNumber(0,canvas.width), randomNumber(0,canvas.height), randomNumber(-.5, .5), randomNumber(-.5, .5));
+    const b3 = new Ball(randomNumber(0,canvas.width), randomNumber(0,canvas.height), randomNumber(-.5, .5), randomNumber(-.5, .5));
 
+    function drawPolygon(obj1, obj2, obj3) {
+      ctx.fillStyle = '#000';
+      ctx.beginPath();
+      ctx.moveTo(obj1.x, obj1.y);
+      ctx.lineTo(obj2.x, obj2.y);
+      ctx.lineTo(obj3.x, obj3.y);
+      ctx.lineTo(obj1.x, obj1.y);
+      ctx.closePath();
+      ctx.fill();
+
+    }
+    
     function draw() {
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        // drawPolygon();
-        
-        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-        }
-        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-            dy = -dy;
-        }
-        
-        x += dx;
-        y += dy;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        b1.drawBall();
+        b2.drawBall();
+        b3.drawBall();
+        drawPolygon(b1, b2, b3);
     }
 
     setInterval(draw, 10);
