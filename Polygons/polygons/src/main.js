4cca97a7c95d9b078321dbb8c0363916e1bdd1ad
Thomas Eykamp <embergameslp@gmail.com>
2021-03-30 11:13:37 -0400

    Cleaning main.js

--- Polygons/polygons/src/main.js
+++ Polygons/polygons/src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Delaunator from 'delaunator' // C:/Users/Thomas/AppData/Local/Microsoft/TypeScript/4.2/node_modules/@types/delaunator/
-
-// Vue.use(Delaunator);
 
 Vue.config.productionTip = false
-// Object.definePrototype(Vue.prototype, '$Delaunator', {value: Delaunator});
-// Vue.prototype.$Delaunator = Delaunator;
 
 new Vue({
   render: h => h(App),
