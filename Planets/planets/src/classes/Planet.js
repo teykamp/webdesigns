3e2ec90b2d1cd469707f56dd8514b54c753b9ef6
Thomas Eykamp <embergameslp@gmail.com>
2021-04-12 17:01:34 -0400

    General Framework

--- Planets/planets/src/classes/Planet.js
+++ Planets/planets/src/classes/Planet.js
@@ -5,12 +5,32 @@ export default class Planet {
         this.dx = dx;
         this.dy = dy;
         this.mass = mass;
-        this.size = 10; // make some function of mass
+        this.radius = 10; // make some function of mass
     }
 
-    updateCoords() {
-        return;
+    moveTowards(planetList) {
+        for (let i=0; i < planetList.length; i++) {
+            const deltaX = this.x - planetList[i].x;
+            const deltaY = this.y - planetList[i].y;
+            const force = this.mass * planetList[i].mass / Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))
+            this.dx += force / this.mass;
+            this.dy += force / this.mass;
+        }
+        if (this.checkDestroy(planetList)) {
+            this.radius = 0;
+            this.mass = 0;
+            this.x = 0;
+            this.y = 0;
+        }
+        // this.x += this.dx;
+        // this.y += this.dy;
     } 
+
+    checkDestroy(planetList) {
+        for (let i = 0; i < planetList.length; i++) {
+            return (this.x == planetList[i].x && this.y == planetList[i].x);
+        }
+    }
 }
 
 
--- Planets/planets/src/components/Canvas.vue
+++ Planets/planets/src/components/Canvas.vue
@@ -24,17 +24,20 @@ export default {
         },
         
         createPoints(num) {
+            // will evenually be called on mouse click and will handle sizes (mouse hold or scroll)
             for (let i=0; i < num; i++) {
-                this.planetList.push(new Planet(this.randomNumber(0, this.width-this.size), this.randomNumber(0, this.height-this.size), this.randomNumber(-.5, .5), this.randomNumber(-.5, .5), this.size));
+                this.planetList.push(new Planet(this.randomNumber(500, this.width-(this.size)-500), this.randomNumber(0, this.height-(this.size*10)), this.randomNumber(-10, 10), this.randomNumber(-10, 10), this.size));
             }
         },
 
         draw() {
             this.ctx.clearRect(0, 0, this.width, this.height)
             for (let i=0; i < this.planetList.length; i++) {
-                this.planetList[i].updateCoords(this.height, this.width, this.planetList);
-                this.ctx.fillStyle = 'green';
-                this.ctx.fillRect(this.planetList[i].x, this.planetList[i].y, this.planetList[i].size, this.planetList[i].size);
+                this.planetList[i].moveTowards(this.planetList);
+                this.ctx.beginPath();
+                this.ctx.arc(this.planetList[i].x, this.planetList[i].y, this.planetList[i].radius, 0, Math.PI * 2, false);
+                this.ctx.fillStyle = "blue";
+                this.ctx.fill();
             }
         },
 
@@ -45,8 +48,7 @@ export default {
             this.height = canvas.height;
             canvas.width = innerWidth-50;
             this.width = canvas.width;
-            // sun
-            this.planetList.push(new Planet(this.width/2, this.height/2, 0, 0, 1000))
+            this.planetList.push(new Planet(this.width/2, this.height/2, 0, 0, 100000000)) // sun
             this.createPoints(2);
 
             setInterval(this.draw, 10);
